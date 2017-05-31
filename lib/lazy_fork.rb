37cacb2d0229c3997bafc1c5cd515e7d3151e8c4
Christian Gregg <c_arlt@hotmail.com>
2017-05-31 20:26:33 +0200

    Little bit of clean up

--- lib/lazy_fork.rb
+++ lib/lazy_fork.rb
@@ -1,55 +1,54 @@
 require "lazy_fork/version"
-
-require "optparse"
 require "io/console"
+require "octokit"
 
 module LazyFork
   class LazyForker
     def initialize(options)
       @options = options
-      @client = nil
 
-      # tell the user their being a lazy fork
-      puts "You lazy forker... v#{LazyFork::VERSION}"
+      # tell the user they're being a lazy fork
+      puts "You lazy forker..."
 
       unless Dir.exist? LAZY_FORK_HOME
         puts "Creating #{LAZY_FORK_HOME}..."
         Dir.mkdir LAZY_FORK_HOME
       end
+
+      authenticate_client
     end
 
-    def be_lazy
-      authenticate_client
-      get_repo(ARGV.first)
-      fork_repo
-      clone_repo
+    def fork
+      clone_repo(fork_repo(get_repo(ARGV.first)))
     end
 
     private
 
-    def fork_repo
+    def fork_repo(repo)
       puts "Forking..."
-      fork_repo = @client.fork(@repo)
-      @forked_repo = Octokit::Repository.new(fork_repo[:full_name])
+      fork_repo = @client.fork(repo)
+      Octokit::Repository.new(fork_repo[:full_name])
     end
 
-    def clone_repo
-      puts "Cloning fork (#{@forked_repo.to_s})..."
-      `git clone #{@forked_repo.url} #{ARGV[1]}`
+    def clone_repo(repo)
+      puts "Cloning fork (#{repo.to_s})..."
+      `git clone #{repo.url} #{ARGV[1]}`
     end
 
     def get_repo(repo)
       begin
-        @repo = Octokit::Repository.new(repo)
+        repo = Octokit::Repository.new(repo)
       rescue  Octokit::InvalidRepository
         puts "Invalid Repository: use owner/name format!"
         abort
       end
 
-      unless @client.repository?(@repo)
-        puts "Repo #{@repo.to_s} could not be found."
+      unless @client.repository?(repo)
+        puts "Repo #{repo.to_s} could not be found."
         abort
       end
+
+      repo
     end
 
     def authenticate_client
